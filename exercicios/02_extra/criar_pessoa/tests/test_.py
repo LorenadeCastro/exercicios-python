48f9782e9679b206e65b11e2a3c78e5d209f20ca
raianecardoso <raianecardoso.rede@gmail.com>
2026-07-09 15:02:48 -0300

    Ajusta teste do exercício cria_pessoa.

--- exercicios/02_extra/criar_pessoa/tests/test_.py
+++ exercicios/02_extra/criar_pessoa/tests/test_.py
@@ -1,21 +1,20 @@
-from metro_milimetro.main import resposta
+from criar_pessoa.main import resposta
 import inspect
 import pytest
 
 
 def test_not_none():
-    assert resposta(10) is not None, "Esperado valor diferente de 'None'"
+    assert resposta('Ana', 30, 'Belo Horizonte') is not None, "Esperado valor diferente de 'None'"
 
 
 def test_type():
-    assert type(resposta(13)) == int or type(resposta(3.2)) == float, "Esperado um inteiro ou float"
+    assert type(resposta('Ana', 30, 'Belo Horizonte')) == dict, "Esperado um dicionário"
 
 
 def test_parameters():
-    assert len(inspect.getfullargspec(resposta).args) == 1, "Assinatura da função deverá receber um parâmetro"
+    assert len(inspect.getfullargspec(resposta).args) == 3, "Assinatura da função deverá receber três parâmetros"
 
 
 def test_options_resposta():
-    assert resposta(10) == 10000, f"Esperado valor 10000"
-    assert resposta(1.2) == 1200, f"Esperado valor 1200"
-    assert resposta(0.93) == 930, f"Esperado valor 930"
+    assert resposta('Ana', 30, 'Belo Horizonte') == {'nome': 'Ana', 'idade': 30, 'cidade': 'Belo Horizonte'}, f"Esperado valor {'nome': 'Ana', 'idade': 30, 'cidade': 'Belo Horizonte'}"
+    assert resposta('João', 25, 'Poços de Caldas') == {'nome': 'João', 'idade': 25, 'cidade': 'Poços de Caldas'}, f"Esperado valor {'nome': 'João', 'idade': 25, 'cidade': 'Poços de Caldas'}"
